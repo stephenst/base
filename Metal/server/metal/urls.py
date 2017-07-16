e0dc8cc3906035305445b3dadc23af6cbec54a5d
Michael Cosgrove <mcosgrove@groupw.com>
2017-07-16 16:45:25 +0000

    Added and updated tables new tables and created methods to populate the tables.  Also included a couple of test urls.  Includes an updated migration file

--- Metal/server/metal/urls.py
+++ Metal/server/metal/urls.py
@@ -4,6 +4,10 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+urlpatterns = [
+    url(r'^$',views.index),
+    url(r'run', views.run_model),
+]
 router.register(r'users', views.UserViewSet, base_name='users')
 router.register(r'perspectives', views.PerspectiveViewSet, base_name='perspectives')
-urlpatterns = router.urls
+urlpatterns += router.urls
