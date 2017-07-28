9f0d2dad181ab01e905b9043f64a829569cd4935
Charles Starkey <cstarkey@groupw.com>
2017-07-28 20:48:49 +0000

    Adding sites, asset route assignments and risk areas to the cesium map

--- Metal/server/metal/admin.py
+++ Metal/server/metal/admin.py
@@ -10,6 +10,9 @@ from .models import AssetResource
 from .models import Route
 from .models import RouteSegment
 from .models import AssetRouteAssignment
+from .models import RiskType
+from .models import RiskArea
+from .models import RiskAreaVertex
 from .models import TimeToFailureDistribution
 
 admin.site.register(Perspective)
@@ -21,4 +24,7 @@ admin.site.register(AssetResource)
 admin.site.register(Route)
 admin.site.register(RouteSegment)
 admin.site.register(AssetRouteAssignment)
+admin.site.register(RiskType)
+admin.site.register(RiskArea)
+admin.site.register(RiskAreaVertex)
 admin.site.register(TimeToFailureDistribution)
