38f0b379d7266868f7134ac49d53f6d19e0be5ef
Charles Starkey <cstarkey@groupw.com>
2017-07-28 20:58:03 +0000

    Adding sites, asset route assignments and risk areas to the cesium map

--- Metal/ui/app/components/scenario/controllers/scanario.controller.js
+++ Metal/ui/app/components/scenario/controllers/scanario.controller.js
@@ -1,10 +1,39 @@
 /**
  * Created by pcharasala on 7/17/17.
  */
+        var earthradius=3440.2769;
+        var sqrt3=Math.sqrt(3);
+        function radians(deg)
+        {
+                return (deg * (Math.PI / 180));
+        };
+        function degLatToNm(degLat)
+        {
+                return ((Math.PI*earthradius*2 / 360) * degLat);
+        };
+        function nmToDegLat(nm)
+        {
+                return (nm / degLatToNm(1));
+        };
+        function degLonToNm(degLon, lat)
+        {
+                var latradius=Math.cos(radians(lat)) * earthradius;
+                return ((Math.PI*latradius*2 / 360) * degLon);
+        };
+        function nmToDegLon(nm, lat)
+        {
+                return (nm / degLonToNm(1, lat));
+        };
+        function nmToLatLon(nmx, nmy) {
+                var lat = nmToDegLat(nmy);
+                var lon = nmToDegLon(nmx, lat);
+                return [ lat, lon ];
+        };
+
 
 scenario
-    .controller('ScenarioController', ['$rootScope', '$scope', 'ScenarioFactory', 'LineFactory','PerspectiveFactory',
-        function($rootScope, $scope, ScenarioFactory, LineFactory, PerspectiveFactory) {
+    .controller('ScenarioController', ['$rootScope', '$scope', 'ScenarioFactory', 'LineFactory','CesiumFactory','PerspectiveFactory',
+        function($rootScope, $scope, ScenarioFactory, LineFactory, CesiumFactory, PerspectiveFactory) {
 
 
         $scope.export = function(eventData) {
@@ -70,58 +99,67 @@ scenario
 
         $scope.getScenario = function(scenarioName){
 
-            $scope.run_model(scenarioName);
-
+            //$scope.run_model(scenarioName);
+            //$scope.getRoutes(scenarioName);
              //$scope.data = [{ key: "Some key", values:[{"label":"77", "value":"22.0"}]}];
             ScenarioFactory.get({id:scenarioName}, function(data){
-                var jsonfileData = data.json_file;
-                //console.log(jsonfileData);
-                var jsonData = angular.fromJson(jsonfileData);
-                //console.log(jsonData);
-                 $scope.selectedScenario = transform(jsonData);
-                 $scope.selectedScenario.forEach(function(e){
-                     $rootScope.viewer.entities.add(e);
-                 });
-                 $rootScope.viewer.zoomTo($rootScope.viewer.entities);
+
             });
 
-            LineFactory.get({id:scenarioName}, function(data){
+            LineFactory.get({id:scenarioName}).$promise.then( function(data){
                 console.log("In Line Factory");
                 var chartKey  = data.key;
                     //console.log(chartKey);
                     var chartDataStr = data.data;
                     var jsonChartData = angular.fromJson(chartDataStr);
-                    //console.log(chartDataStr);
-                    //jsonChartData.forEach(function(e){
-                     //   $scope.graphdata[0].values.push(e);
-                    //});
-                    // $scope.graphdata = [{ key: "Some key", values:[{"label":"77", "value":"22.0"}]}];
-                    //$scope.$apply();
                      $scope.data = [
                         {
                             key: chartKey,
                             values : jsonChartData
                         }
                      ];
+            }).then(function(){
+               CesiumFactory.get({id:scenarioName}).$promise.then( function(data){
+                console.log("Cesium Factory");
+		 // clear the existing map
+                 $rootScope.viewer.entities.removeAll();
+		 // create entities from the map data
+                 $scope.selectedScenario = createCesiumMapEntities(data);
+		 // add entities to the map 
+                 $scope.selectedScenario.forEach(function(e){
+                     $rootScope.viewer.entities.add(e);
+                 });
+		 // zoom into entity location
+                 $rootScope.viewer.zoomTo($rootScope.viewer.entities);
+               });
             });
+
         };
 
-
-
-        var transform = function(metaldata){
+        var createCesiumMapEntities = function(data){
             var viewerData = [];
-            metaldata.sites.forEach(function(e){
+	    // add sites
+            data.sites.forEach(function(e){
                 var site = e;
+                var ll = nmToLatLon(site.latitude, site.longitude);
+		var assetresourcetable = 'none';
+		if (e.asset.asset_resources.length > 0) {
+			assetresourcetable = '<br><table><tr><td>Resource</td><td>Congested Consumption</td><td>Uncongested Consumption</td></tr>';
+			for (var ar = 0; ar < e.asset.asset_resources.length; ar++) {
+			    ares = e.asset.asset_resources[ar];
+	               	    assetresourcetable = assetresourcetable + '<tr><td>' + ares.resource.name + '</td><td>' + ares.contested_consumption  + '</td><td>' + ares.uncontested_consumption + '</td></tr>';
+			}
+			assetresourcetable = assetresourcetable + '</table>';
+		}
                 viewerData.push({
                   id : site.name,
                   name : site.name,
-                    description : 'Location: '+site.location[0]+', '+site.location[1]+'<br>'+
-                    'Risk Type: '+site.risk.type+'<br>'+
-                        'Needed Asset Types: '+site.needed_asset_types,
-                  position : Cesium.Cartesian3.fromDegrees(site.location[1], site.location[0]),
+                    description : 'Location: '+site.latitude+', '+site.longitude+'<br>'+
+                    'Asset Type: '+site.asset.name + '<br>Asset Resources: ' + assetresourcetable,
+                  position : Cesium.Cartesian3.fromDegrees(ll[1], ll[0]),
                   point : {
-                    pixelSize : 5,
-                    color : Cesium.Color.RED,
+                    pixelSize : 6,
+                    color : Cesium.Color.fromCssColorString(site.asset.htmlcolor),
                     outlineColor : Cesium.Color.WHITE,
                     outlineWidth : 2
                   },
@@ -135,6 +173,95 @@ scenario
                   }
                 });
             });
+	    // add threat areas
+            var riskareas = data.risk_areas;
+	    for (var i=0; i<riskareas.length; i++) {
+		var area = riskareas[i];
+		var degarray = [];
+		for (var j=0; j<area.risk_area_verteces.length; j++) {
+		    var vertex = area.risk_area_verteces[j];
+                    var ll = nmToLatLon(vertex.latitude, vertex.longitude);
+		    degarray.push(ll[1]);
+		    degarray.push(ll[0]);
+		}
+		var rcolor = Cesium.Color.fromCssColorString(area.risktype.htmlcolor);
+                viewerData.push({
+			id : area.name,
+			name : area.name,
+			description : 'Risk Type: '+area.risktype.name,
+			polygon : {
+				hierarchy : Cesium.Cartesian3.fromDegreesArray(degarray),
+				material : rcolor.withAlpha(0.5),
+				fill : true,
+				height : 0,
+				outline : true,
+				outlineColor : rcolor,
+				outlineWidth : 5
+			}
+		});
+	    }
+	    // add routes
+            var routedata = data.routes;
+            for (var i=0; i<routedata.length; i++) {
+                var route = routedata[i];
+                if (route.asset_route_assignments.length === 0) {
+                    // no assignments to this route
+                    continue;
+                }
+                var degarray = [];
+                for (var j = 0; j < route.route_segments.length; j++) {
+                    var seg = route.route_segments[j];
+                    if (j === 0) {
+                        var ll0 = nmToLatLon(seg.start_latitude, seg.start_longitude);
+                        degarray.push(ll0[1]);
+                        degarray.push(ll0[0]);
+                    }
+                    var ll1 = nmToLatLon(seg.end_latitude, seg.end_longitude);
+                    degarray.push(ll1[1]);
+                    degarray.push(ll1[0]);
+                }
+                var asset_assignments = '<table>';
+                asset_assignments = asset_assignments + '<tr><td>Asset</td><td>Quantity</td>';
+                for (var r = 0; r < data.resources.length; r++) {
+                    asset_assignments = asset_assignments + '<td>' + data.resources[r].name + '</td>';
+		}
+                asset_assignments = asset_assignments + '</tr>';
+                for (var j = 0; j < route.asset_route_assignments.length; j++) {
+                    var assetinv = route.asset_route_assignments[j];
+		    var asset = assetinv.asset;
+                    asset_assignments = asset_assignments + '<tr><td>' + asset.name + '</td><td>' + assetinv.count + '</td>';
+                    for (var r = 0; r < data.resources.length; r++) {
+			var res = data.resources[r];
+			var found = false;
+			for (var ar = 0; ar < asset.asset_resources.length; ar++) {
+			    ares = asset.asset_resources[ar];
+			    if (ares.resource.id === res.id) {
+                    		asset_assignments = asset_assignments + '<td>' + ares.transport_capacity  + '</td>';
+				found = true;
+				break;
+			    }
+			}
+			if (!found) {
+                    		asset_assignments = asset_assignments + '<td>0</td>';
+			}
+		    }
+                    asset_assignments = asset_assignments + '</tr>';
+                }
+                asset_assignments = asset_assignments + '</table>';
+                viewerData.push({
+                    id: route.name,
+                    name: route.name,
+                    description: 'length: ' + route.distance + '<br>Asset Assignments:<br>' + asset_assignments,
+                    polyline: {
+                        positions: Cesium.Cartesian3.fromDegreesArray(degarray),
+                        width: 5,
+                        material: new Cesium.PolylineGlowMaterialProperty({
+                            glowPower: 0.2,
+                            color: Cesium.Color.BLUE
+                        })
+                    }
+                });
+            }
             return viewerData;
         };
 
